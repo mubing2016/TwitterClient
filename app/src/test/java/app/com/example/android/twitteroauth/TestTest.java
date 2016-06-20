faad76bb1c5bc91f4317e50f85be7cffb6f36d84
Mubing Liu <mubingliu@Mubings-MacBook-Pro.local>
2016-06-20 09:47:32 -0400

    change app name

---
+++ app/src/test/java/app/com/example/android/twitteroauth/TestTest.java
@@ -0,0 +1,10 @@
+package app.com.example.android.twitteroauth;
+
+import org.junit.Test;
+
+//public class TestTest {
+//    @Test
+//    public void testOneOne() {
+//        assertEquals(targetValue, class.method(args));
+//    }
+//}
